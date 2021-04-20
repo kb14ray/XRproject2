42cb05baf7508ae59aa62dc9cdeabb29ff3aa627
chtruiBen <hsiang0418@gmail.com>
2021-04-20 15:59:11 +0800

    add rollball

---
+++ rollball/rollball/Assets/Scripts/player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(movehorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement);
+    }
+}
